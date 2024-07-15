ac65bfacbd898491ed26501d46f66c12e181048b
notlelouch <notlelouchvb@gmail.com>
2024-07-15 20:50:23 +0530

    fixing errors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
@@ -17,8 +19,14 @@ async function bootstrap() {
     .addTag('Todo')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
+  SwaggerModule.setup('/swagger', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
@@ -16,7 +25,8 @@ export class TodoController {
   @Post()
   @ApiOperation({
     summary: 'Create a new todo item',
-    description: 'This endpoint allows an authenticated user to create a new todo item. The user must provide the necessary details such as title and description in the request body.'
+    description:
+      'This endpoint allows an authenticated user to create a new todo item. The user must provide the necessary details such as title and description in the request body.',
   })
   create(@Body() createTodoDto: CreateTodoDto, @UserEmail() userEmail: string) {
     return this.todoService.create(createTodoDto, userEmail);
@@ -27,7 +37,8 @@ export class TodoController {
   @Get()
   @ApiOperation({
     summary: 'Get all todo items',
-    description: 'This endpoint retrieves all todo items for the authenticated user. The user\'s email is used to fetch their specific todo items.'
+    description:
+      "This endpoint retrieves all todo items for the authenticated user. The user's email is used to fetch their specific todo items.",
   })
   findAll(@UserEmail() userEmail: string) {
     return this.todoService.findAll(userEmail);
@@ -38,7 +49,8 @@ export class TodoController {
   @Get(':id')
   @ApiOperation({
     summary: 'Get a specific todo item',
-    description: 'This endpoint retrieves a specific todo item by its ID. The ID should be provided as a path parameter.'
+    description:
+      'This endpoint retrieves a specific todo item by its ID. The ID should be provided as a path parameter.',
   })
   findOne(@Param('id') id: string) {
     return this.todoService.findOne(+id);
@@ -49,7 +61,8 @@ export class TodoController {
   @Patch(':id')
   @ApiOperation({
     summary: 'Update a specific todo item',
-    description: 'This endpoint allows an authenticated user to update a specific todo item by its ID. The ID should be provided as a path parameter, and the updated details should be provided in the request body.'
+    description:
+      'This endpoint allows an authenticated user to update a specific todo item by its ID. The ID should be provided as a path parameter, and the updated details should be provided in the request body.',
   })
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todoService.update(+id, updateTodoDto);
@@ -60,7 +73,8 @@ export class TodoController {
   @Delete(':id')
   @ApiOperation({
     summary: 'Delete a specific todo item',
-    description: 'This endpoint allows an authenticated user to delete a specific todo item by its ID. The ID should be provided as a path parameter.'
+    description:
+      'This endpoint allows an authenticated user to delete a specific todo item by its ID. The ID should be provided as a path parameter.',
   })
   remove(@Param('id') id: string) {
     return this.todoService.remove(+id);
