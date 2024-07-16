cda5a5db6f6564de1a80d7259e932ab4e5845fbe
notlelouch <notlelouchvb@gmail.com>
2024-07-16 15:42:34 +0530

    fixing deployment errors

--- src/main.ts
+++ src/main.ts
@@ -19,14 +19,29 @@ async function bootstrap() {
     .addTag('Todo')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/swagger', app, document, {
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
-    ],
-  });
+  SwaggerModule.setup('api', app, document);
+
+  // SwaggerModule.setup('/swagger', app, document, {
+  //   customCssUrl:
+  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+  //   customJs: [
+  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+  //   ],
+  // });
   await app.listen(3000);
 }
 bootstrap();
+
+// const config = new DocumentBuilder()
+//   .setTitle('Cats example')
+//   .setDescription('The cats API description')
+//   .setVersion('1.0')
+//   .addTag('cats')
+//   .build();
+// const document = SwaggerModule.createDocument(app, config);
+// SwaggerModule.setup('api', app, document);
+
+// await app.listen(3000);
+// }
+// bootstrap();
